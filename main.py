c1cd1171a30f5c13281ba9b5ebd9b3f7c7c9caf8
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-02-22 22:08:08 +0400

    something start working

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import time
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+import requests
 
 bot_father_token="6078167339:AAFZGPXLHWjZ7ElMK0q2iR-ZTTR-6OizyWg"
+logging.basicConfig(level=logging.INFO)
 
-my_bot=Bot(tojen=bot_father_token)
-my_dispatcher=Dispatcher()
+my_bot=Bot(token=bot_father_token)
+my_dispatcher=Dispatcher(bot=my_bot)
+
+
+@my_dispatcher.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    await message.reply("Hello, I'm a bot")
+
+if __name__ == '__main__':
+    executor.start_polling(my_dispatcher,skip_updates=True)
