bf19f173e3f8e8b217e73b6f3096ee19ffc4e9c1
woodyjohnson13 <johnsonwoody13@gmail.com>
2023-02-22 21:25:03 +0400

    init

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 
 bot_father_token="6078167339:AAFZGPXLHWjZ7ElMK0q2iR-ZTTR-6OizyWg"
+
+my_bot=Bot(tojen=bot_father_token)
+my_dispatcher=Dispatcher()
